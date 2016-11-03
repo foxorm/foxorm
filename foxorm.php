26131f7322bed7f7d029ec87668a8413f0d0c7e1
jo <jo@surikat.pro>
2016-11-03 19:04:40 +0100

    rebuild

--- foxorm.php
+++ foxorm.php
@@ -301,6 +301,10 @@ abstract class DataSource implements \ArrayAccess,\Iterator,\JsonSerializable{
 			$type = $default;
 		else
 			$type = $this->entityClassDefault;
+		
+		if(!isset($array['_modified']))
+			$array['_modified'] = true;
+		
 		$obj = $this->entityFactory($type,$array);
 		return $obj;
 	}
@@ -545,7 +549,7 @@ abstract class DataSource implements \ArrayAccess,\Iterator,\JsonSerializable{
 				$properties[$k] = $v;
 			}
 		}
-
+		
 		foreach($oneNew as $t=>$ones){
 			foreach($ones as $one){
 				$this[$t][] = $one;
@@ -636,7 +640,7 @@ abstract class DataSource implements \ArrayAccess,\Iterator,\JsonSerializable{
 				$this->addFK($typ,$targetType,$property,$targetProperty,$isDep);
 			}
 		}
-		
+
 		if($modified){
 			$this->trigger($type,'afterPut',$obj);
 			$this->trigger($type,'unserializeColumns',$obj);
